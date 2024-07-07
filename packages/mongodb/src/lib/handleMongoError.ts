cab59f2950843684e910e6a5b517f8709840546c
Daniel Gördes <d.goerdes@overlap.at>
2024-07-07 11:27:51 +0200

    clean up

--- packages/mongodb/src/lib/handleMongoError.ts
+++ packages/mongodb/src/lib/handleMongoError.ts
@@ -9,14 +9,14 @@ export const handleMongoError = (error: any): Exception => {
         return new InvalidInputException(
             error.message,
             error.errInfo?.['details'],
-        );
+        ).fromError(error);
     } else if (error.code && error.code === 2) {
-        return new InvalidInputException(error.message);
+        return new InvalidInputException().fromError(error);
     } else if (error.code && error.code === 11000) {
         return new InvalidInputException(error.message, {
             keyValue: error['keyValue'],
-        });
+        }).fromError(error);
     } else {
-        return new GenericException(error.message);
+        return new GenericException().fromError(error);
     }
 };
