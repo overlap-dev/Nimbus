28c07f764a052ce6fc0cd9b836a7b76a9bd2c255
Daniel Gördes <d.goerdes@overlap.at>
2024-10-30 14:11:13 +0100

    feat(mongodb): adds more error details for bulkwrite errors

--- packages/mongodb/src/lib/handleMongoError.ts
+++ packages/mongodb/src/lib/handleMongoError.ts
@@ -6,10 +6,14 @@ import {
 
 export const handleMongoError = (error: any): Exception => {
     if (error.code && error.code === 121) {
-        return new InvalidInputException(
-            error.message,
-            error.errInfo?.['details'],
-        ).fromError(error);
+        return new InvalidInputException(error.message, {
+            code: error.code,
+            ...(error.errInfo?.['details'] && {
+                details: error.errInfo?.['details'],
+            }),
+            ...(error.writeErrors && { writeErrors: error.writeErrors }),
+            ...(error.result && { result: error.result }),
+        }).fromError(error);
     } else if (error.code && error.code === 2) {
         return new InvalidInputException().fromError(error);
     } else if (error.code && error.code === 11000) {
