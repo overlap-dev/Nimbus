1f56b0bf72c5eaeedc8bae0be254b1b2c35426d2
Daniel Gördes <d.goerdes@overlap.at>
2024-11-11 15:57:26 +0100

    feat(mongodb): adds mongodb client reconnecting

--- packages/mongodb/src/lib/mongodbClient.ts
+++ packages/mongodb/src/lib/mongodbClient.ts
@@ -2,15 +2,41 @@ import { MongoClient, MongoClientOptions } from 'mongodb';
 
 const sharedMongoClients: Record<string, MongoClient> = {};
 
-export const getMongoClient = async (
+export const initMongoClient = async (
     url: string,
     options?: MongoClientOptions,
 ): Promise<MongoClient> => {
+    async function connect() {
+        await sharedMongoClients[url].connect();
+    }
+
     if (!sharedMongoClients[url]) {
-        sharedMongoClients[url] = await MongoClient.connect(
+        sharedMongoClients[url] = new MongoClient(
             url,
             Object.assign({}, options),
         );
+
+        sharedMongoClients[url].on('serverOpening', (event) => {
+            console.log(
+                `MongoDB :: serverOpening :: connection to ${event.address} established.`,
+            );
+        });
+        sharedMongoClients[url].on('serverClosed', async (event) => {
+            console.log(
+                `MongoDB :: serverClosed :: connection to ${event.address} lost.\nReconnecting...`,
+            );
+            await connect();
+        });
+
+        await connect();
+    }
+
+    return sharedMongoClients[url];
+};
+
+export const getMongoClient = (url: string): MongoClient => {
+    if (!sharedMongoClients[url]) {
+        throw new Error(`MongoClient not initialized!`);
     }
 
     return sharedMongoClients[url];
