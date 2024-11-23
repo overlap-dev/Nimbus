95b3a7a41e282bb918bc2d2e4f7c38a618407ba1
Daniel Gördes <d.goerdes@overlap.at>
2024-11-23 17:52:25 +0100

    feat(mongodb): rework the mongodb repository implementation

--- packages/mongodb/src/lib/repository.ts
+++ packages/mongodb/src/lib/repository.ts
@@ -1,20 +1,45 @@
-import {
-    GenericException,
-    type Repository,
-    type RepositoryReadOptions,
-} from '@nimbus/core';
-import type { Collection, Document } from 'mongodb';
+import type {
+    BulkWriteOptions,
+    Collection,
+    CountDocumentsOptions,
+    DeleteOptions,
+    Document,
+    Filter,
+    FindOptions,
+    InsertOneOptions,
+    ReplaceOptions,
+    Sort,
+    WithId,
+} from 'mongodb';
 import { ObjectId } from 'mongodb';
 import type { ZodType } from 'zod';
+import { bulkWrite } from './crud/bulkWrite.ts';
+import { countDocuments } from './crud/countDocuments.ts';
+import { deleteMany } from './crud/deleteMany.ts';
 import { deleteOne } from './crud/deleteOne.ts';
 import { find } from './crud/find.ts';
 import { findOne } from './crud/findOne.ts';
+import { insertMany } from './crud/insertMany.ts';
 import { insertOne } from './crud/insertOne.ts';
 import { replaceOne } from './crud/replaceOne.ts';
-import { MongoJSON } from './mongoJSON.ts';
 
-export class MongoDBRepository<TEntity extends Record<string, any>>
-    implements Repository<TEntity> {
+/**
+ * Repository for interacting with a MongoDB Collection
+ *
+ * Why do we not implement a generic repository interface?
+ *
+ * Implementing a generic repository interface is incredibly difficult in terms of
+ * mapping filter patterns to the Database without the user having to change a
+ * filter input when switching databases in the background.
+ *
+ * So we feel it is better to make the underlying data store obvious to the user
+ * and give the user a way to interact with the repository in a way that feels
+ * natural to the underlying data store.
+ *
+ * @param collection - MongoDB Collection
+ * @param entityType - Zod Type used to parse the received data and ensure type safety
+ */
+export class MongoDBRepository<TEntity extends WithId<Record<string, any>>> {
     private _collection: Collection<Document>;
     private _entityType: ZodType;
 
@@ -24,45 +49,74 @@ export class MongoDBRepository<TEntity extends Record<string, any>>
     }
 
     private _mapDocumentToEntity(doc: Document): TEntity {
-        return doc as TEntity;
+        return this._entityType.parse(doc);
     }
 
     private _mapEntityToDocument(item: TEntity): Document {
         return item as Document;
     }
 
-    public read({
-        limit,
-        skip,
-        sortDirection,
-        sortField,
-        filterString,
-    }: RepositoryReadOptions) {
-        const filter = MongoJSON.parse(filterString ?? '{}');
-
-        return find({
-            collection: this._collection,
-            filter: filter,
-            limit: limit,
-            skip: skip,
-            sort: {
-                [sortField ?? 'createdAt']: sortDirection ?? 'asc',
-            },
-            mapDocument: this._mapDocumentToEntity,
-            outputType: this._entityType,
-        });
-    }
-
-    public readById(id: string) {
+    public findOne({
+        filter,
+    }: {
+        filter: Filter<Document>;
+    }) {
         return findOne({
             collection: this._collection,
-            filter: { _id: new ObjectId(id) },
+            filter,
             mapDocument: this._mapDocumentToEntity,
             outputType: this._entityType,
         });
     }
 
-    public async create(item: TEntity) {
+    public find({
+        filter,
+        limit,
+        skip,
+        sort,
+        project,
+        options,
+    }: {
+        filter: Filter<Document>;
+        limit?: number;
+        skip?: number;
+        sort?: Sort;
+        project?: Document;
+        options?: FindOptions;
+    }) {
+        return find({
+            collection: this._collection,
+            filter,
+            limit,
+            skip,
+            sort,
+            project,
+            mapDocument: this._mapDocumentToEntity,
+            outputType: this._entityType,
+            options,
+        });
+    }
+
+    public countDocuments({
+        filter,
+        options,
+    }: {
+        filter: Filter<Document>;
+        options?: CountDocumentsOptions;
+    }) {
+        return countDocuments({
+            collection: this._collection,
+            filter,
+            options,
+        });
+    }
+
+    public async insertOne({
+        item,
+    }: {
+        item: TEntity;
+        options?: InsertOneOptions;
+    }) {
         await insertOne({
             collection: this._collection,
             document: this._mapEntityToDocument(item),
@@ -71,34 +125,95 @@ export class MongoDBRepository<TEntity extends Record<string, any>>
         return item;
     }
 
-    public async update(item: TEntity) {
-        if (typeof item._id !== 'string') {
-            throw new GenericException(
-                'MongoDBRepository.update :: Item is missing the _id field',
-            );
-        }
+    public async insertMany({
+        items,
+        options,
+    }: {
+        items: TEntity[];
+        options?: BulkWriteOptions;
+    }) {
+        await insertMany({
+            collection: this._collection,
+            documents: items.map(this._mapEntityToDocument),
+            options,
+        });
 
+        return items;
+    }
+
+    public async replaceOne({
+        item,
+        options,
+    }: {
+        item: TEntity;
+        options?: ReplaceOptions;
+    }) {
         await replaceOne({
             collection: this._collection,
             filter: { _id: new ObjectId(item._id) },
             replacement: this._mapEntityToDocument(item),
+            options,
         });
 
         return item;
     }
 
-    public async delete(item: TEntity) {
-        if (typeof item._id !== 'string') {
-            throw new GenericException(
-                'MongoDBRepository.delete :: Item is missing the _id field',
-            );
+    public async replaceMany({
+        items,
+        options,
+    }: {
+        items: TEntity[];
+        options?: BulkWriteOptions;
+    }) {
+        if (items.length > 0) {
+            const operations = items.map((item) => ({
+                replaceOne: {
+                    filter: { _id: new ObjectId(item._id) },
+                    replacement: this._mapEntityToDocument(item),
+                },
+            }));
+
+            await bulkWrite({
+                collection: this._collection,
+                operations: operations,
+                options,
+            });
         }
 
+        return items;
+    }
+
+    public async deleteOne({
+        item,
+        options,
+    }: {
+        item: TEntity;
+        options?: DeleteOptions;
+    }) {
         await deleteOne({
             collection: this._collection,
             filter: { _id: new ObjectId(item._id) },
+            options,
         });
 
         return item;
+    }
+
+    public async deleteMany({
+        items,
+        options,
+    }: {
+        items: TEntity[];
+        options?: DeleteOptions;
+    }) {
+        await deleteMany({
+            collection: this._collection,
+            filter: {
+                _id: { $in: items.map((item) => new ObjectId(item._id)) },
+            },
+            options,
+        });
+
+        return items;
     }
 }
