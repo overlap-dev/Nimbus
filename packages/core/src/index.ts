757b65c1c757dae88f642764fbe5722d563534a2
Daniel Gördes <d.goerdes@overlap.at>
2024-11-23 18:04:47 +0100

    chore(core): release 0.9.0

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,5 +3,4 @@ export * from './lib/command/index.ts';
 export * from './lib/event/index.ts';
 export * from './lib/exception/index.ts';
 export * from './lib/query/index.ts';
-export * from './lib/repository/index.ts';
 export * from './lib/router/index.ts';
