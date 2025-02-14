f4bb4732f1cd9b8a974292687be101c72d3abfb9
Daniel Gördes <d.goerdes@overlap.at>
2025-02-14 12:54:23 +0100

    chore(examples): adds example for a command with no response body

--- examples/the-expense/src/account/shell/account.router.ts
+++ examples/the-expense/src/account/shell/account.router.ts
@@ -1,8 +1,10 @@
 import { NimbusOakRouter } from '@nimbus/oak';
 import { AddAccountCommand } from '../core/commands/addAccount.ts';
+import { DeleteAccountCommand } from '../core/commands/deleteAccount.ts';
 import { GetAccountQuery } from '../core/queries/getAccount.ts';
 import { ListAccountsQuery } from '../core/queries/listAccounts.ts';
 import { addAccountHandler } from './commands/addAccount.handler.ts';
+import { deleteAccountHandler } from './commands/deleteAccount.handler.ts';
 import { getAccountHandler } from './queries/getAccount.handler.ts';
 import { listAccountsHandler } from './queries/listAccounts.handler.ts';
 
@@ -27,4 +29,11 @@ accountRouter.command(
     'ADD_ACCOUNT',
     AddAccountCommand,
     addAccountHandler,
+);
+
+accountRouter.command(
+    '/delete-account',
+    'DELETE_ACCOUNT',
+    DeleteAccountCommand,
+    deleteAccountHandler,
 );
---
+++ examples/the-expense/src/account/shell/commands/deleteAccount.handler.ts
@@ -0,0 +1,25 @@
+import { type RouteHandler } from '@nimbus/core';
+import { ObjectId } from 'mongodb';
+import {
+    deleteAccount,
+    DeleteAccountCommand,
+} from '../../core/commands/deleteAccount.ts';
+import { accountRepository } from '../account.repository.ts';
+
+export const deleteAccountHandler: RouteHandler<any> = async (
+    command: DeleteAccountCommand,
+) => {
+    let account = await accountRepository.findOne({
+        filter: {
+            _id: new ObjectId(command.data._id),
+        },
+    });
+
+    account = deleteAccount(account, command.metadata.authContext);
+
+    await accountRepository.deleteOne({ item: account });
+
+    return {
+        statusCode: 204,
+    };
+};
