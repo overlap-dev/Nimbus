60aa55de308b072ca4aa485e0fb24dd03506ccd2
Daniel Gördes <d.goerdes@overlap.at>
2025-09-04 18:38:29 +0200

    adds client package with todo

---
+++ packages/client/src/lib/sdk.ts
@@ -0,0 +1,6 @@
+// TODO: implement some kind of SDK creator
+// Support the messages handling like creating commands, events and queries.
+
+export const sdk = () => {
+    console.log("To be implemented");
+};
