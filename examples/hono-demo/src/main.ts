c50d7663a4334c2cf3da8aab8be81cf24c9af2a8
Daniel Gördes <d.goerdes@overlap.at>
2026-01-15 21:19:24 +0100

    feat(hono): adds hono package and demo

---
+++ examples/hono-demo/src/main.ts
@@ -0,0 +1,55 @@
+import '@std/dotenv/load';
+import process from 'node:process';
+import {
+    getLogger,
+    jsonLogFormatter,
+    parseLogLevel,
+    prettyLogFormatter,
+    setupLogger,
+} from '@nimbus/core';
+import { logger } from '@nimbus/hono';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { secureHeaders } from 'hono/secure-headers';
+import { compress } from 'hono/compress';
+
+setupLogger({
+    logLevel: parseLogLevel(process.env.LOG_LEVEL),
+    formatter: process.env.LOG_FORMAT === 'pretty'
+        ? prettyLogFormatter
+        : jsonLogFormatter,
+    useConsoleColors: process.env.LOG_FORMAT === 'pretty',
+});
+
+const app = new Hono();
+
+app.use(logger({
+    enableTracing: true,
+    tracerName: 'api',
+}));
+
+app.use(cors());
+
+app.use(secureHeaders());
+
+app.use(compress());
+
+app.get('/health', (c) => {
+    return c.json({ status: 'ok' });
+});
+
+if (process.env.PORT) {
+    const port = parseInt(process.env.PORT);
+
+    Deno.serve({ port }, app.fetch);
+    getLogger().info({
+        category: 'API',
+        message: `Started application on port ${port}`,
+    });
+} else {
+    getLogger().critical({
+        category: 'API',
+        message:
+            `Could not start the application! Please define a valid port vienvironment variable.`,
+    });
+}
