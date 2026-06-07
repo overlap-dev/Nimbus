0c59f72890ab3d5e4bba9799382b5cc2f1f298fe
devn-ch <233639015+devn-ch@users.noreply.github.com>
2026-06-07 03:45:58 +0200

    fix: use project path

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,7 @@ import remarkGfm from 'remark-gfm';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://devn-ch.github.io/EventFabric',
 	integrations: [
 		starlight({
 			title: 'EventFabric CQRS Framework',
